900eee991a2cf0cd9cd5c6c7aa2593ec91adf850
Rares Cosma <office@rarescosma.com>
2014-03-30 14:37:03 +0300

    up again.

--- provision-wp-stack.sh
+++ provision-wp-stack.sh
@@ -2,10 +2,7 @@
 
 # nginx / fpm / npm
 apt-get install -y nginx php5-fpm php5-cli php5-mysql nodejs
-
-for x in `ls -1 /etc/php5/conf.d`; do echo "\n\nFile: $x\n"; cat /etc/php5/conf.d/$x; done
-
-cp nginx.conf /etc/nginx/nginx.conf
+cp etc/nginx.conf /etc/nginx/nginx.conf
 /etc/init.d/php5-fpm restart
 /etc/init.d/nginx restart
 
