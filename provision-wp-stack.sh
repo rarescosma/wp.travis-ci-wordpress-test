a4ab458265b6078dea799acce6f82e51be9d545e
Rares Cosma <office@rarescosma.com>
2014-03-30 12:48:53 +0300

    up #5

--- provision-wp-stack.sh
+++ provision-wp-stack.sh
@@ -3,15 +3,14 @@
 rm -rf /etc/apt/sources.list.d/*
 apt-get update
 
-apt-get install nginx
-apt-get install php5-fpm
+apt-get install nginx php5-fpm
 
 cp nginx.conf /etc/nginx/nginx.conf
 /etc/init.d/nginx restart
 
-rm -rf /etc/php5/fpm/pool.d/*
-cp fpm.conf /etc/php5/fpm/pool.d/www.conf
-/etc/init.d/php5-fpm restart
+#rm -rf /etc/php5/fpm/pool.d/*
+#cp fpm.conf /etc/php5/fpm/pool.d/www.conf
+#/etc/init.d/php5-fpm restart
 
 echo "127.0.0.1 wp.l" >> /etc/hosts
 
